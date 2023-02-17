c1fd3bf9b1e1bdcf552ed15553bba601726c7770
Naveen Sai <118335656+NaveenSai1605@users.noreply.github.com>
2023-02-17 17:15:10 +0530

    Update SlackNotifications.groovy

--- vars/SlackNotifications.groovy
+++ vars/SlackNotifications.groovy
@@ -1,6 +1,7 @@
-def notifyBuild(String buildStatus = 'STARTED') 
-{
+def call(String buildStatus = 'STARTED') {
   // build status of null means successful
+  //This is the condition which we are checking weather buildStatus is SUCCESSFULL or not.
+ //This line updated to show the Eclipse with GitHub demo
   buildStatus =  buildStatus ?: 'SUCCESS'
 
   // Default values
@@ -16,13 +17,11 @@ def notifyBuild(String buildStatus = 'STARTED')
   } else if (buildStatus == 'SUCCESS') {
     color = 'GREEN'
     colorCode = '#00FF00'
-	} else {
+  } else {
     color = 'RED'
     colorCode = '#FF0000'
   }
 
-  // Send notifications
+  // Calling the slackSend function to Send notifications.
   slackSend (color: colorCode, message: summary)
 }
-
-
